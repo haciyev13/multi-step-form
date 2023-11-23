ce2f95daf30ba556d4f7bf49a01d6c03900900ec
haciyev13 <ehedhaciyev001@gmail.com>
2023-11-23 16:05:03 +0400

    Task add

---
+++ assets/js/app.js
@@ -0,0 +1,74 @@
+// let secImgCard = document.querySelectorAll(".section__form-img-card");
+// console.log(secImgCard);
+// let secImgCard1 = document.querySelector(".section__form-img-card-1");
+// let secImgCard2 = document.querySelector(".section__form-img-card-2");
+// let secImgCard3 = document.querySelector(".section__form-img-card-3");
+// let secImgCard4 = document.querySelector(".section__form-img-card-4");
+
+// let secCardCircle1 = document.querySelector(".section__form-img-card-circle-1");
+// let secCardCircle2 = document.querySelector(".section__form-img-card-circle-2");
+// let secCardCircle3 = document.querySelector(".section__form-img-card-circle-3");
+// let secCardCircle4 = document.querySelector(".section__form-img-card-circle-4");
+
+// let secInfo1 = document.querySelector(".section__form-info-1");
+// let secInfo2 = document.querySelector(".section__form-info-2");
+// let secInfo3 = document.querySelector(".section__form-info-3");
+// let secInfo4 = document.querySelector(".section__form-info-4");
+
+
+
+// secImgCard1.addEventListener("click", function () {
+//   secCardCircle1.classList.toggle("click");
+//   secCardCircle2.classList.remove("click");
+//   secCardCircle3.classList.remove("click");
+//   secCardCircle4.classList.remove("click");
+//   secInfo1.style.display = "block";
+//   secInfo2.style.display = "none";
+//   secInfo3.style.display = "none";
+//   secInfo4.style.display = "none";
+// });
+// secImgCard2.addEventListener("click", function () {
+//   secCardCircle2.classList.toggle("click");
+//   secCardCircle1.classList.remove("click");
+//   secCardCircle3.classList.remove("click");
+//   secCardCircle4.classList.remove("click");
+//   secInfo2.style.display = "block";
+//   secInfo1.style.display = "none";
+//   secInfo3.style.display = "none";
+//   secInfo4.style.display = "none";
+// });
+// secImgCard3.addEventListener("click", function () {
+//   secCardCircle3.classList.toggle("click");
+//   secCardCircle2.classList.remove("click");
+//   secCardCircle1.classList.remove("click");
+//   secCardCircle4.classList.remove("click");
+//   secInfo3.style.display = "block";
+//   secInfo2.style.display = "none";
+//   secInfo1.style.display = "none";
+//   secInfo4.style.display = "none";
+// });
+// secImgCard4.addEventListener("click", function () {
+//   secCardCircle4.classList.toggle("click");
+//   secCardCircle2.classList.remove("click");
+//   secCardCircle3.classList.remove("click");
+//   secCardCircle1.classList.remove("click");
+//   secInfo4.style.display = "block";
+//   secInfo2.style.display = "none";
+//   secInfo3.style.display = "none";
+//   secInfo1.style.display = "none";
+// });
+
+// console.log(secCardCircle);
+
+// secImgCard.forEach((elem) => {
+//   elem.addEventListener("click", function () {
+//     console.log(this);
+//     this.firstElementChild.classList.toggle("click");
+//     this.addEventListener("click", function () {
+//       console.log(secInfo1.style.display = "block");
+//       secInfo2.style.display = "none";
+//       secInfo3.style.display = "none";
+//       secInfo4.style.display = "none";
+//     })
+//   });
+// });
